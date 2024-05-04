43895d099ab63e56d5c9e3051b2d86d99059e525
Alejandro Pachas <alejandropachas1@gmail.com>
2024-05-03 21:35:04 -0500

    [src/router/index.js] indenting file for better readability

--- src/router/index.js
+++ src/router/index.js
@@ -1,25 +1,33 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import ForoView from '../views/ForoView.vue'
-import MaterialView from '../views/MaterialView.vue'
-import TutoriasView from '../views/TutoriasView.vue'
-import SuscripcionesView from '../views/SuscripcionesView.vue'
-import CrearCuentaView from '../views/CrearCuentaView.vue';
-import RepositoriosView from '../views/RepositoriosView.vue';
-import inicioSesion from "@/views/InicioSesion.vue";
+import { createRouter, createWebHistory } from "vue-router"
+import HomeView from "../views/HomeView.vue"
+import ForoView from "../views/ForoView.vue"
+import MaterialView from "../views/MaterialView.vue"
+import TutoriasView from "../views/TutoriasView.vue"
+import SuscripcionesView from "../views/SuscripcionesView.vue"
+import CrearCuentaView from "../views/CrearCuentaView.vue"
+import RepositoriosView from "../views/RepositoriosView.vue"
+import inicioSesion from "@/views/InicioSesion.vue"
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.VITE_BASE_URL),
   routes: [
-    { path: '/', name: 'home', component: HomeView },
-    { path: '/foro', name: 'foro', component: ForoView },
-    { path: '/repositorios', name: 'repositorios', component: RepositoriosView},
-    { path: '/material', name: 'material', component: MaterialView },
-    { path: '/tutorias', name: 'tutorias', component: TutoriasView },
-    { path: '/suscripciones', name: 'suscripciones', component: SuscripcionesView },
-    { path: '/iniciosesion', name: 'iniciosesion', component:inicioSesion},
-    { path: '/crearcuenta', name: 'crearcuenta', component: CrearCuentaView },
-  ]
-});
+    { path: "/", name: "home", component: HomeView },
+    { path: "/foro", name: "foro", component: ForoView },
+    {
+      path: "/repositorios",
+      name: "repositorios",
+      component: RepositoriosView,
+    },
+    { path: "/material", name: "material", component: MaterialView },
+    { path: "/tutorias", name: "tutorias", component: TutoriasView },
+    {
+      path: "/suscripciones",
+      name: "suscripciones",
+      component: SuscripcionesView,
+    },
+    { path: "/iniciosesion", name: "iniciosesion", component: inicioSesion },
+    { path: "/crearcuenta", name: "crearcuenta", component: CrearCuentaView },
+  ],
+})
 
 export default router
